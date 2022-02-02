cd1f85930675dfba1e3cf918bc8550f31b5d64c9
ak1920 <ak1920@ic.ac.uk>
2022-02-02 01:12:21 +0000

    Add tests for assign, read, free and return statements and restyle all tests

--- src/test/scala/valid_programs_tests/statement_tests.scala
+++ src/test/scala/valid_programs_tests/statement_tests.scala
@@ -12,39 +12,141 @@ class StatParserTest extends AnyFlatSpec with AppendedClues{
     
     info("STATEMENT TESTS")
 
-    "Skip statements" should "parse successfully and produce a correct AST" in {
-    fully(statement).parse("skip") should matchPattern {case Success(Skip()) =>} withClue(" Success(Skip())")
-
+    "Skip statements" should "parse successfully and produce a correct AST" in 
+    {
+        fully(statement).parse("skip") should matchPattern {
+        case Success
+        (
+            Skip()
+        ) =>} withClue(" Success(Skip())")
     }
 
-    "Assignment to array literal" should "parse successfully and produce a correct AST" in {
+    "Assignment to array literal" should "parse successfully and produce a correct AST" in 
+    {
         info("singleton array")
-        fully(statement).parse("int [] x = [1]") should matchPattern 
-            {case Success(AssignType(ArrayType(IntType(), 1), Ident("x"), ArrayLiter(List(IntLiter(1))))) 
-            =>} withClue(" Success(AssignType(ArrayType(IntType(), 1), Ident(\"x\"), ArrayLiter(List(IntLiter(1)))))")
+        fully(statement).parse("int [] x = [1]") should matchPattern {
+        case Success
+        (
+            AssignType
+            (
+                ArrayType(IntType(), 1),
+                Ident("x"), 
+                ArrayLiter(List(IntLiter(1)))
+            )
+        ) =>} withClue(" Success(AssignType(ArrayType(IntType(), 1), " +
+                       "Ident(\"x\"), ArrayLiter(List(IntLiter(1)))))")
 
         info("array with multiple elements")
-        fully(statement).parse("bool [] arr = [true, false, true]") should matchPattern 
-            {case Success(AssignType(ArrayType(BoolType(), 1),Ident("arr"), ArrayLiter(List(BoolLiter(true), BoolLiter(false), BoolLiter(true))))) 
-            =>} withClue(" Success(AssignType(ArrayType(BoolType(), 1), Ident(\"arr\"), ArrayLiter(List(BoolLiter(true), BoolLiter(false), BoolLiter(true)))))")
+        fully(statement).parse("bool [] arr = [true, false, true]") should matchPattern {
+        case Success
+        (
+            AssignType
+            (
+                ArrayType(BoolType(), 1),
+                Ident("arr"), 
+                ArrayLiter(List(BoolLiter(true), BoolLiter(false), BoolLiter(true)))
+            )
+        ) =>} withClue(" Success(AssignType(ArrayType(BoolType(), 1), Ident(\"arr\"), " +
+                       "ArrayLiter(List(BoolLiter(true), BoolLiter(false), BoolLiter(true)))))")
 
     }
 
-    "Newpair statements" should "parse successfully and produce a correct AST" in pending
+    "Assignment to newpair statements" should "parse successfully and produce a correct AST" in {
+        fully(statement).parse("pair(int, int) p = newpair(1,2)") should matchPattern {
+        case Success
+        (
+            AssignType
+            (
+                PairType(IntType(), IntType()),
+                Ident("p"), 
+                NewPair(IntLiter(1), IntLiter(2))
+            )
+        ) =>} withClue(" Success(AssignType(PairType(IntType(), IntType()), " +
+                       "Ident(\"p\"), NewPair(IntLiter(1), IntLiter(2))))")
+    }
 
-    //arglist?
+    "Assignment to call statement" should "parse successfully and produce a correct AST" in {
+        info("call function with no arguments")
+        fully(statement).parse("bool res = call foo()") should matchPattern{
+        case Success
+        (
+            AssignType
+            (
+                BoolType(),
+                Ident("res"), 
+                Call(Ident("foo"), List())
+            )
+        ) =>} withClue(" Success(AssignType(BoolType(), Ident(\"res\"), " +
+                       "Call(Ident(\"foo\"), List())))")
 
-    "Call statements" should "parse successfully and produce a correct AST" in pending
+        info("call function with arguments")
+        fully(statement).parse("int res = call foo(1, true)") should matchPattern{
+        case Success
+        (
+            AssignType
+            (
+                IntType(),
+                Ident("res"), 
+                Call(Ident("foo"), List(IntLiter(1), BoolLiter(true)))
+            )
+        ) =>} withClue(" Success(AssignType(IntType(), Ident(\"res\"), " +
+                       "Call(Ident(\"foo\"), List(IntLiter(1), BoolLiter(true)))))")
+    }
+            
 
-    "Pair elements" should "parse successfully and produce a correct AST" in pending
+    "Assignment to expression statements" should "parse successfully and produce a correct AST" in {
 
-    "Assign statements" should "parse successfully and produce a correct AST" in pending
+        info("assign array element to expression")
+        fully(statement).parse("arr[0] = 42") should matchPattern{
+        case Success
+        (
+            Assign
+            (
+                ArrayElem(Ident("arr"), List(IntLiter(0))), 
+                IntLiter(42)
+            )
+        ) =>} withClue(" Success(Assign(ArrayElem(Ident(\"arr\")," +
+                       " List(IntLiter(0))), IntLiter(42)))")
 
-    "Read statements" should "parse successfully and produce a correct AST" in pending
+        info("assign pair element to expression")
+        fully(statement).parse("fst p = true") should matchPattern
+            {case Success(Assign(Fst(Ident("p")), BoolLiter(true)))
+        =>} withClue(" Success(Assign(Fst(Ident(\"p\")), BoolLiter(true)))")
 
-    "Free statements" should "parse successfully and produce a correct AST" in pending
+        info("assign identifier to expression")
+        fully(statement).parse("var = 1 + 2") should matchPattern
+            {case Success(Assign(Ident("var"), Add(IntLiter(1), IntLiter(2))))
+        =>} withClue(" Success(Assign(Ident(\"var\"), Add(IntLiter(1), IntLiter(2))))")
+    }
 
-    "Return statements" should "parse successfully and produce a correct AST" in pending
+    "Read statements" should "parse successfully and produce a correct AST" in {
+        info("read into identifier")
+        fully(statement).parse("read input") should matchPattern
+            {case Success(Read(Ident("input")))
+        =>} withClue(" Success(Read(Ident(\"input\")))")
+
+        info("read into pair elem")
+        fully(statement).parse("read snd p") should matchPattern
+            {case Success(Read(Snd(Ident("p"))))
+        =>} withClue(" Success(Read(Snd(Ident(\"p\"))))")
+
+        info("read into array elem")
+        fully(statement).parse("read arr[0][2]") should matchPattern
+            {case Success(Read(ArrayElem(Ident("arr"), List(IntLiter(0), IntLiter(2)))))
+        =>} withClue(" Success(Read(ArrayElem(Ident(\"arr\"), List(IntLiter(0), IntLiter(2)))))")
+    }
+
+    "Free statements" should "parse successfully and produce a correct AST" in {
+        fully(statement).parse("free arr[0]") should matchPattern
+            {case Success(Free(ArrayElem(Ident("arr"), List(IntLiter(0)))))
+        =>} withClue(" Success(Free(ArrayElem(Ident(\"arr\"), List(IntLiter(0)))))")
+    }
+
+    "Return statements" should "parse successfully and produce a correct AST" in {
+        fully(statement).parse("return result") should matchPattern
+            {case Success(Return(Ident("result")))
+        =>} withClue(" Success(Return(Ident(\"result\")))")
+    }
 
     "Exit statements" should "parse successfully and produce a correct AST" in pending
 

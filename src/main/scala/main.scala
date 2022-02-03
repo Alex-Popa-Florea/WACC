df5a34056d5aa12686d6cefa22da470f2ee13a94
Arunmolithevar, Thaarukan <thaarukan.arunmolithevar20@imperial.ac.uk>
2022-02-03 11:33:59 +0000

    [ta620] Fromat main to return exit codes

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,13 +1,26 @@
 package wacc
-import parsley.Parsley, Parsley._
+
 object main {
     import parser._
     import parsley.{Success, Failure}
+    import scala.io.Source
+    import ast._
+  
+
     def main(args: Array[String]) = {
-        result.parse(args.head) match {
-            case Success(x) => println(s"${args.head} = $x")
-            case Failure(err) => println(err)
+       assert(args.head != "")
+        result.parse(Source.fromFile(args.head).getLines.toList.mkString("\n")) match {
+            case Success(x) => {
+                println(s"${args.head} = $x")
+            }
+            case Failure(err) => {
+                println(err)
+                sys.exit(200)
+            }
         }
     }
 
-}
+
+
+
+}   

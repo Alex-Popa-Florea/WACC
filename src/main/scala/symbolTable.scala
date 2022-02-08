89813e1032868686b465eb0d7c898eaeaa871284
Alexander Popa Florea <alexander.popa-florea20@imperial.ac.uk>
2022-02-08 10:27:35 +0000

    Create symbol table to store variable to type mapping

---
+++ src/main/scala/symbolTable.scala
@@ -0,0 +1,31 @@
+package wacc
+
+import parsley.Parsley, Parsley._
+import ast._
+import types._
+
+object symbolTable {
+    class SymbolTable(parent: Option[SymbolTable]) {
+        var variableMap: Map[(String, Boolean), TypeCheck] = Map()
+
+        def add(variable: (String, Boolean), varType: TypeCheck): Boolean = {
+            variableMap.get(variable) match {
+                case None => 
+                    variableMap += (variable -> varType)
+                    true
+                case _ => false
+            }
+        }
+
+        def find(variable: (String, Boolean)): Option[TypeCheck] = {
+            var foundType = variableMap.get(variable)
+            foundType match {
+                case None => parent match {
+                    case None => None
+                    case _ => parent.get.find(variable)
+                }
+                case _ => foundType
+            }
+        }
+    }
+}

b5576f229d62138bf1600b5ddc229ee44c5d1757
Arunmolithevar, Thaarukan <thaarukan.arunmolithevar20@imperial.ac.uk>
2022-02-08 16:50:25 +0000

    Remove Ops and add explanations

--- src/main/scala/parser.scala
+++ src/main/scala/parser.scala
@@ -7,7 +7,7 @@ object parser {
     import lexer.implicits.implicitLexeme
     import lexer._
     import ast._
-    import parsley.expr.{precedence, Ops, InfixL, InfixR, NonAssoc, Prefix, SOps}
+    import parsley.expr.{precedence, InfixL, InfixR, NonAssoc, Prefix, Ops}
     import parsley.combinator._
     import parsley.errors.combinator.ErrorMethods
     
@@ -33,6 +33,40 @@ object parser {
     Variables are Expressions!
     """
 
+    val explainExpr =s"""Expressions:
+    Expressions are a combinations of values and operators.
+    Some operators take 2 arguments and others just 1 describe below:
+    Not, Negate a bool  ${makeGreen("!<bool>")}
+    Negation, negate a number ${makeGreen("-<int>")}
+    Length, get the length of an array ${makeGreen("len <array>")}
+    Ordinal, gives the ASCII value of the character ${makeGreen("ord <variable>")}
+    Character, gives the ASCII charcter from the given number ${makeGreen("chr <int>")}
+    Multiply, gives the  ${makeGreen("<int> * <int>")}
+    Divide, gives the integer divison of 2 ints  ${makeGreen("<int> / <int>")}
+    Mod, gives the remainder of dividing 2 ints ${makeGreen("<int> % <int>")}
+    Add, gives the addition of 2 ints ${makeGreen("<int> + <int>")}
+    Sub, gives the subtraction of 2 ints ${makeGreen("<int> - <int>")}
+    Greater Than, gives true if LHS greater than RHS ${makeGreen("<int> > <int>")}
+    Greater Than Or Equal To, gives true if LHS greater than or equal to RHS ${makeGreen("<int> >= <int>")}
+    Less Than, gives true if LHS less than RHS ${makeGreen("<int> < <int>")}
+    Less Than Or Equal To, gives true if LHS less than or equal toRHS ${makeGreen("<int> <= <int>")}
+    Equals, gives true if LHS is equal RHS ${makeGreen("<variable> == <variable>")}
+    Not Equals, gives true if LHS is not equal RHS ${makeGreen("<variable> != <variable>")}
+    Logical And,gives true if both bools are true ${makeGreen("<bool> && <bool>")}
+    Logical Or,gives true if one of the bools are true ${makeGreen("<bool> || <bool>")}
+    """
+
+    val explainLHS = s"""Left hand side of an assignment, left of the equals:
+    Can be an array element: ${makeGreen("<array>[<position>] =")}
+    Can be a variable: ${makeGreen("<variable> = ")}
+    Can be a pair element: ${makeGreen("fst <pair> =")} 
+                           ${makeGreen("snd <pair> =")}
+    """
+    val explainPairElem = s"""Acessing pairs using fst and snd are used to access pairs:
+    to get the first element ${makeGreen("fst <pair>")}
+    to get the second element ${makeGreen("snd <pair>")}
+    """
+
     private def count(p: =>Parsley[_]): Parsley[Int] = p.foldLeft(0)((n, _) => n + 1)
 
     private lazy val ident = Ident(VARIABLE)
@@ -41,17 +75,18 @@ object parser {
     private lazy val intLiter = (attempt(option("+")) ~> IntLiter(INTEGER))
     private lazy val boolLiter = BoolLiter(("true" ~> pure(true)) <|> ("false" ~> pure(false)))
     private lazy val stringLiter = StrLiter(STRING)
-    private lazy val arrayLiter = "[" ~> ArrayLiter(sepBy(expr, ",")) <~ "]"
+    private lazy val arrayLiter = ("[" ~> ArrayLiter(sepBy(expr, ",")) <~ "]").hide.explain(s"Array can be constructed by having\n  elements, if any, seperated by commas surrounded in square brackets ${makeGreen("[<expression>,<expression>...]")}\n")
+
     private lazy val pairLiter = (PairLiter <# "null")
 
     private lazy val arrayElem = ArrayElem(ident, endBy1("[" ~> expr, "]"))
 
-    private lazy val pairElem: Parsley[PairElem] = ("fst" ~> Fst(expr)) <|> ("snd" ~> Snd(expr))
-    private lazy val newPair = "newpair" ~> NewPair("(" ~> expr, "," ~> expr <~ ")")
+    private lazy val pairElem: Parsley[PairElem] = ("fst" ~> Fst(expr)) <|> ("snd" ~> Snd(expr)).hide
+    private lazy val newPair = "newpair" ~> NewPair("(" ~> expr, "," ~> expr <~ ")").hide.explain(s"Pairs are contrusted using newpair: ${makeGreen("newpair (<expression>, <expression>)")}\n")
 
-    private lazy val types: Parsley[Type] = attempt((baseType <|> pairType) <~ notFollowedBy("[")) <|> arrayType
+    lazy val types: Parsley[Type] = (attempt((baseType <|> pairType) <~ notFollowedBy("[")) <|> arrayType).hide
 
-    private lazy val baseType: Parsley[BaseType] = ((IntType <# "int") <|> (StrType <# "string") <|> (BoolType <# "bool") <|> (CharType <# "char"))
+    private lazy val baseType: Parsley[BaseType] = ((IntType <# "int") <|> (StrType <# "string") <|> (BoolType <# "bool") <|> (CharType <# "char")).hide
     private lazy val arrayType = ArrayType((baseType <|> pairType), count("[" <~ "]"))
     private lazy val pairType = PairType("pair" ~> "(" ~> pairElemType <~ ",", pairElemType <~ ")")
     private lazy val pairElemType: Parsley[PairElemType] = attempt(baseType <~ notFollowedBy("[")) <|> arrayType <|> (Pair <# "pair")
@@ -61,12 +96,12 @@ object parser {
     private lazy val param = Parameter(types, " " ~> Ident(VARIABLE))
     private lazy val params = sepBy(param, ",")
     private lazy val function = attempt(Function(types, Ident(VARIABLE), "(" ~> params <~ ")", "is" ~> nestedStatement))
-    private lazy val functions = endBy(function, "end")
+    lazy val functions = endBy(function, "end")
 
-    private lazy val call = Call("call" ~> ident, "(" ~> arglist <~ ")")
+    private lazy val call = Call("call" ~> ident, "(" ~> arglist <~ ")").hide.explain(s"To call a function: ${makeGreen("call <name of function>(<argument>,<argument>)")}\n")
     
-    private lazy val assignLHS: Parsley[AssignLHS] = attempt(arrayElem) <|> ident <|> pairElem
-    private lazy val assignRHS = expr <|> arrayLiter <|> newPair <|> pairElem <|> call
+    private lazy val assignLHS: Parsley[AssignLHS] = (attempt(arrayElem) <|> ident <|> pairElem).hide
+    private lazy val assignRHS = expr.explain(explainExpr) <|> arrayLiter <|> newPair <|> pairElem.explain(explainPairElem) <|> call
     
     private lazy val nestedStatement = sepBy1(statement, ";")
     lazy val statement: Parsley[Statement] = 
@@ -84,25 +119,25 @@ object parser {
         NestedBegin("begin" ~> nestedStatement <~ "end")
         ).hide.explain(explainStatement)
         
-    private lazy val program = "begin" ~> (Begin(functions, nestedStatement)) <~ "end"
+    lazy val program = "begin" ~> (Begin(functions, nestedStatement)) <~ "end"
     private lazy val atom =  
         "(" ~> expr <~ ")" <|> attempt(arrayElem) <|> ident <|> charLiter <|> intLiter <|> boolLiter <|> stringLiter <|> pairLiter 
 
     lazy val expr: Parsley[Expr] = precedence[Expr](atom)(
-        SOps(Prefix)(Not  <# "!"),
-        SOps(Prefix)(Neg  <# "-"),
-        SOps(Prefix)(Len  <# "len"),
-        SOps(Prefix)(Ord  <# "ord"),
-        SOps(Prefix)(Chr  <# "chr"),
-        SOps(InfixL)(Mul  <# "*",  Div <# "/"),
-        SOps(InfixL)(Mod  <# "%"),
-        SOps(InfixL)(Add  <# "+",  Sub <# "-"),
-        SOps(NonAssoc)(GT <# ">",  GTE <# ">=",
+        Ops(Prefix)(Not  <# "!"),
+        Ops(Prefix)(Neg  <# "-"),
+        Ops(Prefix)(Len  <# "len"),
+        Ops(Prefix)(Ord  <# "ord"),
+        Ops(Prefix)(Chr  <# "chr"),
+        Ops(InfixL)(Mul  <# "*",  Div <# "/"),
+        Ops(InfixL)(Mod  <# "%"),
+        Ops(InfixL)(Add  <# "+",  Sub <# "-"),
+        Ops(NonAssoc)(GT <# ">",  GTE <# ">=",
                       LT <# "<",  LTE <# "<=",
                       EQ <# "==", NEQ <# "!="),
-        SOps(InfixR)(And  <# "&&"),
-        SOps(InfixR)(Or   <# "||")
+        Ops(InfixR)(And  <# "&&"),
+        Ops(InfixR)(Or   <# "||")
         
-    )
+    ).label("operators")
     val result = fully(program)
 }

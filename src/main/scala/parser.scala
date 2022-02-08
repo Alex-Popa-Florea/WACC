838f1ee920d90367325d3148f23dc500b3c048b2
Arunmolithevar, Thaarukan <thaarukan.arunmolithevar20@imperial.ac.uk>
2022-02-08 15:04:47 +0000

    Change Op to SOps in precedence table

--- src/main/scala/parser.scala
+++ src/main/scala/parser.scala
@@ -7,7 +7,7 @@ object parser {
     import lexer.implicits.implicitLexeme
     import lexer._
     import ast._
-    import parsley.expr.{precedence, Ops, InfixL, InfixR, NonAssoc, Prefix}
+    import parsley.expr.{precedence, Ops, InfixL, InfixR, NonAssoc, Prefix, SOps}
     import parsley.combinator._
     
     private def count(p: =>Parsley[_]): Parsley[Int] = p.foldLeft(0)((n, _) => n + 1)
@@ -26,28 +26,28 @@ object parser {
     private lazy val pairElem: Parsley[PairElem] = ("fst" ~> Fst(expr)) <|> ("snd" ~> Snd(expr))
     private lazy val newPair = "newpair" ~> NewPair("(" ~> expr, "," ~> expr <~ ")")
 
-    lazy val types: Parsley[Type] = attempt((baseType <|> pairType) <~ notFollowedBy("[")) <|> arrayType
+    private lazy val types: Parsley[Type] = attempt((baseType <|> pairType) <~ notFollowedBy("[")) <|> arrayType
 
     private lazy val baseType: Parsley[BaseType] = ((IntType <# "int") <|> (StrType <# "string") <|> (BoolType <# "bool") <|> (CharType <# "char"))
     private lazy val arrayType = ArrayType((baseType <|> pairType), count("[" <~ "]"))
     private lazy val pairType = PairType("pair" ~> "(" ~> pairElemType <~ ",", pairElemType <~ ")")
-    private lazy val pairElemType: Parsley[PairElemType] = (Pair <# "pair") <|> attempt(baseType <~ notFollowedBy("[")) <|> arrayType
+    private lazy val pairElemType: Parsley[PairElemType] = attempt(baseType <~ notFollowedBy("[")) <|> arrayType <|> (Pair <# "pair")
     
     private lazy val arglist = sepBy(expr, ",")
-                                               
-    private lazy val param = Parameter(types, Ident(VARIABLE))
+    
+    private lazy val param = Parameter(types, " " ~> Ident(VARIABLE))
     private lazy val params = sepBy(param, ",")
     private lazy val function = attempt(Function(types, Ident(VARIABLE), "(" ~> params <~ ")", "is" ~> nestedStatement))
-    lazy val functions = endBy(function, "end")
-                                               
+    private lazy val functions = endBy(function, "end")
+
     private lazy val call = Call("call" ~> ident, "(" ~> arglist <~ ")")
-                                               
+    
     private lazy val assignLHS: Parsley[AssignLHS] = attempt(arrayElem) <|> ident <|> pairElem
     private lazy val assignRHS = expr <|> arrayLiter <|> newPair <|> pairElem <|> call
-                                               
+    
     private lazy val nestedStatement = sepBy1(statement, ";")
-    lazy val statement: Parsley[Statement] =  
-        (Skip <# "skip") <|>                 
+    lazy val statement: Parsley[Statement] = 
+        (Skip <# "skip") <|> 
         AssignType(types, ident, "=" ~> assignRHS) <|> 
         Assign(assignLHS, "=" ~> assignRHS) <|>
         Read("read" ~> assignLHS) <|>
@@ -60,24 +60,26 @@ object parser {
         While("while" ~> expr, "do" ~> nestedStatement <~ "done") <|>
         NestedBegin("begin" ~> nestedStatement <~ "end")
         
-    lazy val program = "begin" ~> (Begin(functions, nestedStatement)) <~ "end"
+        
+    private lazy val program = "begin" ~> (Begin(functions, nestedStatement)) <~ "end"
     private lazy val atom =  
         "(" ~> expr <~ ")" <|> attempt(arrayElem) <|> ident <|> charLiter <|> intLiter <|> boolLiter <|> stringLiter <|> pairLiter 
 
     lazy val expr: Parsley[Expr] = precedence[Expr](atom)(
-        Ops(Prefix)(Not  <# "!"),
-        Ops(Prefix)(Neg  <# "-"),
-        Ops(Prefix)(Len  <# "len"),
-        Ops(Prefix)(Ord  <# "ord"),
-        Ops(Prefix)(Chr  <# "chr"),
-        Ops(InfixL)(Mul  <# "*",  Div <# "/"),
-        Ops(InfixL)(Mod  <# "%"),
-        Ops(InfixL)(Add  <# "+",  Sub <# "-"),
-        Ops(NonAssoc)(GT <# ">",  GTE <# ">=",
+        SOps(Prefix)(Not  <# "!"),
+        SOps(Prefix)(Neg  <# "-"),
+        SOps(Prefix)(Len  <# "len"),
+        SOps(Prefix)(Ord  <# "ord"),
+        SOps(Prefix)(Chr  <# "chr"),
+        SOps(InfixL)(Mul  <# "*",  Div <# "/"),
+        SOps(InfixL)(Mod  <# "%"),
+        SOps(InfixL)(Add  <# "+",  Sub <# "-"),
+        SOps(NonAssoc)(GT <# ">",  GTE <# ">=",
                       LT <# "<",  LTE <# "<=",
                       EQ <# "==", NEQ <# "!="),
-        Ops(InfixR)(And  <# "&&"),
-        Ops(InfixR)(Or   <# "||")       
+        SOps(InfixR)(And  <# "&&"),
+        SOps(InfixR)(Or   <# "||")
+        
     )
     val result = fully(program)
 }

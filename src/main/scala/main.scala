583c3b64b4fdca0f3fb8f26d52c465416f7532d4
ak1920 <ak1920@ic.ac.uk>
2022-02-08 14:53:15 +0000

    Change main to use Parsley's parseFromFile method

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,13 +3,14 @@ package wacc
 object main {
     import parser._
     import parsley.{Success, Failure}
+    import parsley.io.ParseFromIO
     import scala.io.Source
     import ast._
-  
+    import java.io.File;
 
     def main(args: Array[String]) = {
        assert(args.head != "")
-        result.parse(Source.fromFile(args.head).getLines.toList.mkString("\n")) match {
+        result.parseFromFile(new File(args.head)).get match {
             case Success(x) => {
                 println(s"${args.head} = $x")
             }

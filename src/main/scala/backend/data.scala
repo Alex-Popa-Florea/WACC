e63e947a39d0aff3c365514e3f38cba79b357af6
Ishita Mahatme <im620@texel14.doc.ic.ac.uk>
2022-03-02 15:22:23 +0000

    Correct warning in tests to print escape line characters in output files

--- src/main/scala/backend/data.scala
+++ src/main/scala/backend/data.scala
@@ -6,9 +6,17 @@ object data {
     sealed trait Data extends Line
     case class Msg(id: Int, size: Int, text: String) extends Data {
         override def toString() : String = {
+            var newText = text.replace("\u0000", raw"\0")
+                              .replace("\b", raw"\b")
+                              .replace("\t", raw"\t")
+                              .replace("\n", raw"\n")
+                              .replace("\f", raw"\f")
+                              .replace("\r", raw"\r")
+                              .replace("\"", """\"""")
+                              .replace("\'", raw"\'")
             s"""|msg_${id}:
                 |    .word ${size}
-                |    .ascii    "${text.replace("\"", """\"""")}"
+                |    .ascii    "${newText}"
                 |""".stripMargin
         }
     }
